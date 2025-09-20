b56569b77860d8dfecc55914dbe7eea16ffc6f91
Francis Poon <francool97@gmail.com>
2025-09-20 16:06:24 -0400

    Adding in parent components

---
+++ project_manager/project_manager.gd
@@ -0,0 +1,15 @@
+class_name ProjectManager
+extends Node
+
+# This will have a project manager data and will be called to update the data
+# or load it
+@export var data: ProjectManagerData
+
+func get_project_resource(project_id: int):
+	pass
+
+func update_project(project_data: ProjectData):
+	pass
+
+func load_data():
+	pass
