1622801ad656d144c20889814790f6e30e668c88
Francis Poon <francool97@gmail.com>
2025-10-05 16:20:32 -0400

    testing why project reference is loading wrong

--- resources/project_reference.gd
+++ resources/project_reference.gd
@@ -7,7 +7,7 @@ var project_resource_path: String
 
 func _init(p_id: int = -1, p_name: String = "Untitled Project", p_project_resource_path: String = ""):
 	id = p_id
-	if id == -1:
-		id = randi()
+	#if id == -1:
+		#id = randi()
 	name = p_name
 	project_resource_path = "{0}{1}.tres".format([Globals.get_project_save_dir(), id])
