7bf40390a0c43d6c75936d8cdc784ff91a534585
Francis Poon <francool97@gmail.com>
2025-10-05 20:20:29 -0400

    Fixed projec reference not loading properly

--- resources/project_reference.gd
+++ resources/project_reference.gd
@@ -1,9 +1,9 @@
 class_name ProjectReference
 extends Resource
 
-var id: int
-var name: String
-var project_resource_path: String
+@export var id: int
+@export var name: String
+@export var project_resource_path: String
 
 func _init(p_id: int = -1, p_name: String = "Untitled Project", p_project_resource_path: String = ""):
 	id = p_id
